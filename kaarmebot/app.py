5aa0c63d4de81af02d4464a2884642cc38c7eb42
Jaakko Pallari <jkpl@lepovirta.org>
2012-09-08 01:31:33 +0300

    add_route no longer adds plugins, only routes

--- kaarmebot/app.py
+++ kaarmebot/app.py
@@ -15,7 +15,9 @@ class plugin_config:
         settings = self.__dict__.copy()
 
         def callback(scanner, name, ob):
-            scanner.add_plugin(ob, name, settings)
+            if settings.get('name') is None:
+                settings['name'] = name
+            scanner.add_plugin(ob, **settings)
 
         info = venusian.attach(wrapped, callback, category='plugins')
         if info.scope == 'class' and settings.get('attr') is None:
@@ -27,26 +29,25 @@ class plugin_config:
 class BotApp:
     def __init__(self, channels, servers, nickname, real_name,
                  plugin_settings=None):
-        self._plugins = {}
-        self._scanner = venusian.Scanner(add_plugin=self._add_plugin)
+        self.plugins = {}
         self.dispatcher = MessageDispatcher(plugin_settings)
         self.bot = BotCore(channels, servers, nickname, real_name,
                            self.dispatcher.msg)
+        self._scanner = venusian.Scanner(add_plugin=self.add_plugin)
 
-    def add_route(self, regex, handler, msgtypes=None, attr=None):
-        if isinstance(handler, basestring):
-            handler, settings = self._plugins[handler]
-            msgtypes = (tuple(msgtypes or []) +
-                        tuple(settings.get('msgtypes', [])))
-            attr = attr or settings.get('attr')
+    def add_route(self, regex, name, msgtypes=None, attr=None):
+        handler, settings = self.plugins[name]
+        msgtypes = (tuple(msgtypes or []) +
+                    tuple(settings.get('msgtypes', [])))
+        attr = attr or settings.get('attr')
         self.dispatcher.add_route(regex, handler, msgtypes, attr)
 
     def scan(self, module):
         self._scanner.scan(module)
 
-    def _add_plugin(self, handler, name, settings):
+    def add_plugin(self, handler, name, **settings):
         name = settings.get('name') or name
-        self._plugins[name] = (handler, settings)
+        self.plugins[name] = (handler, settings)
 
     def start(self):
         self.bot.start()
